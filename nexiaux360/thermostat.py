ddda868cad5400201e23ea58685462f21943cd74
dmld08865 <dmlorah@gmail.com>
2024-10-12 20:16:26 -0500

    Update thermostat.py
    Models for UX360 contain different versions base on when implemented.  From what I can see base on mine, what we have in common it appears that if the code looks like it checks what the model starts with it might cover more version of the UX360 controller.

--- nexiaux360/thermostat.py
+++ nexiaux360/thermostat.py
@@ -1,4 +1,3 @@
-"""Nexia Themostat."""
 
 from __future__ import annotations
 
@@ -682,7 +681,7 @@ class NexiaThermostat:
         )
 
         """ Support for UX360 Thermostat """
-        if self.get_model() == "TSYS2C60A2VVUEA":
+        if self.get_model().startswith('TSYS2C60A2VVU'):
             print(f"UX360 End Point      : {end_point}")
             if end_point == "fan_mode":  # update_thermostat_fan_mode
                 url = self._get_thermostat_deep_key(
